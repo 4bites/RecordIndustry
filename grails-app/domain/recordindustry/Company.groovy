060a96bf323aa1f63412ba70de533c581a12f2a6
jiseruk <jiseruk@ubuntu.ubuntu-domain>
2010-05-10 08:56:45 -0300

    cambios a domains

--- grails-app/domain/recordindustry/Company.groovy
+++ grails-app/domain/recordindustry/Company.groovy
@@ -5,12 +5,15 @@ class Company {
     String description
     String cuit
     String address
-    String homepage 
-    static constraints = {
+    String homepage
+    Integer code
+    static hasMany = [entities:LegalEntity, persons:Person] 
+    static constraint = {
         name(blank: false)
 	description(blank: false, maxSize:1000)
   	cuit(blank: false, matches: /^\d{2}\-\d{8}\-\d{1}$/, unique: true)
 	address(blank: false, maxSize:100)
 	homepage(url: true)
+	entities(minSize:1)
     }
 }
--- grails-app/domain/recordindustry/Person.groovy
+++ grails-app/domain/recordindustry/Person.groovy
@@ -1,9 +1,11 @@
 package recordindustry
 
 class Person {
-    String name
+    String firstName
+    String lastName
     static hasMany = [entities:LegalEntity]
-
+    
     static constraints = {
+        cuit(blank: false, matches: /^\d{2}\-\d{8}\-\d{1}$/, unique: true)
     }
 }
